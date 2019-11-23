06c75e78bcbae008cc3dd0d8773b827c9ba1ca1f
David Winters <davewinters1987@gmail.com>
2019-11-23 20:56:13 +0000

    Game canvas built

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log('Hello JS');
+// Constants
+const Canvas_Boarder_Colour = 'black';
+const Canvas_Background_Colour = 'white';
+
+// Get canvas elements
+var gameCanvas = document.getElementById("gameCanvas");
+
+// 2D Context
+var ctx = gameCanvas.getContext("2d");
+
+// Canvas fill colour
+ctx.fillStyle = Canvas_Background_Colour;
+
+// Border colour
+ctx.strokeStyle = Canvas_Boarder_Colour;
+
+// Canvas rect
+ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+// Draw border
+ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
